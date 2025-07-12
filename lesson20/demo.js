4024a51ec0139eb1ba673c81f2bc22ff4337df9e
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-07-12 11:13:35 +0530

    project20

---
+++ lesson20/demo.js
@@ -0,0 +1,35 @@
+ function gotowashroom(){
+    console.log( "hi mam good morning, can i go to washroom")
+    console.log("please mam, please let me go.")
+ }
+
+ gotowashroom()
+ gotowashroom()
+ gotowashroom()
+ gotowashroom() 
+ gotowashroom()
+ gotowashroom()
+ gotowashroom()
+
+ function sum(a,b,c){
+    var result = a + b + c
+   console. log(result)
+ }
+
+ sum(4, 6, 10)
+ sum(2, 1, 0)
+
+
+ function multiply(a,b,c,d){
+    var r = a *  b * c * d 
+    return r
+ } 
+ 
+  var k = multiply(2,4,6,8)
+  console.log(k)
+ 
+
+
+
+
+
