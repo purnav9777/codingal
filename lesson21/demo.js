4b3f36b6621d4ef3f8a839b583186fa2f511f3e9
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-07-19 11:24:56 +0530

    project22

---
+++ lesson21/demo.js
@@ -0,0 +1,7 @@
+ var a = Math.random()
+ console.log(a)
+
+  var b = {x: 78, sayHellow: function(){return "yo yo is the new hello for pop industary"}}
+
+    var c = b.sayHellow()
+    console. log(c)
---
+++ lesson22/demo.js
@@ -0,0 +1,18 @@
+ var a ={gogo: 56, rafay: 'basketball king raffay', asia:['bangladesh','india','china']
+ ,projectDone: false, printsiddhes: function(){return 'siddeesh full name '},
+ siddheshDemand: 9+4 } 
+
+ console.log(a.rafay)
+ console.log(a.gogo)
+ console.log(a.projectDone)
+ console.log(a.siddheshDemand)
+ console.log(a.printsiddhes())
+
+ console.log(Math.random())
+ console.log(Math.pt)
+
+ var b = 'ball and bat'
+ console.log(b.toLocaleUpperCase())
+
+ var c = 2
+ console.log(c.toExponential())
