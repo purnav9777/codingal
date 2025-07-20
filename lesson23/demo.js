94cd3ff831d2f8ae88fbe859ebc0cc00a8582bc3
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-07-20 11:35:54 +0530

    project23

---
+++ lesson23/demo.js
@@ -0,0 +1,34 @@
+ var a =['dog','cat','deer','lion', 'elephant' ]
+
+ console.log( a[0])
+ console.log( a[2])
+ console.log( a[3])
+ 
+ var groupage =[44,22,14,87,93,11,9,4,74]
+ console.log( a[0])
+ console.log( a[1])
+ console.log( a[2])
+ console.log( a[3])
+
+ var spiderman =[true,false,true,false,true,false,true]
+ var objarray =[{a: 39838, b:34823, leo: 'yooyoo i am leo'},{cow:'givesa milk',
+ milk:  'gives lactose protein'}, { name: 'doha',income:'high'}]
+
+ console.log(objarray[1].milk)
+
+ console.log( a.join ("_"))
+ a.pop()
+
+ console.log( a.join ("_"))
+
+ function sum (a,b,c){
+    return a+b+c
+ }
+
+function average(a,b,c){ 
+return sum(a,b,c)/3
+}
+
+
+const result = average(4,8,20)
+console.log (result)
