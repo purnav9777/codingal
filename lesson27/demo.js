199dfec1ea0561e53d851b9ad00f9a49d5654638
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-08-03 11:32:20 +0530

    project27

---
+++ lesson27/demo.js
@@ -0,0 +1,15 @@
+var a =[4,9,2,20,7,5,3,0,-20,80,72]
+a.sort(function(a,b){return a-b})
+
+
+a.short((a,b)=> a-b)
+
+console.log(a)
+
+a.short((a,b)=> a-b)
+
+console.log(a.map(0=>0*10))
+
+console.log(a.map(function(e){return e*10}))
+
+console.log(a)
