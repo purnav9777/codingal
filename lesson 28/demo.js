831d9be15f2221af4084b196ae3a6dbcbcfac8f2
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-08-17 11:25:46 +0530

    web project 1

---
+++ lesson 28/demo.js
@@ -0,0 +1,19 @@
+var a ='76'
+console.log(typeof(a))
+
+a = Number(a)
+
+console.log(typeof(a))
+
+console.log(typeof(nan))
+
+var b =[6,8,9,20]
+
+console.log(typeof(b))
+
+try{
+    //error code
+    console.log(" rafay rafay balleh balleh ")
+}catch(err){
+    console.log()
+}
