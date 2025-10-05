eba3b762abfbf316af4af78cd5a037092317b865
purnav9777 <rajaramkhanvilkar08@gimail.com>
2025-10-05 11:27:12 +0530

    python8

---
+++ python6/demo.py
@@ -0,0 +1,20 @@
+abc = ['America', 'Brazil', ' India']
+
+print(abc[0])
+print(abc[1])
+print(abc[2])
+
+print(len(abc))
+
+
+print(abc [1:3])
+
+abc.append('China')
+print(abc)
+
+
+abc.pop()
+
+print(abc)
+
+
---
+++ python8/demo.py
@@ -0,0 +1,11 @@
+class sweet_dish:
+    def __init__(self, name, price, area):
+        self.name = name
+        self.price = price
+        self.area = area
+
+
+
+kheer_obj = sweet_dish("kheer", 800, "goa-pakistan-up-haryana")
+
+print(kheer_obj.area)
